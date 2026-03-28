92b605d81390fd3d0607be025f4dbc2ed7f3bb7e
Evan McLain <emclain@example.com>
2026-03-28 07:54:40 +0000

    test: add inline test for await expression (jedi-u9g)

--- test/refactor/inline.py
+++ test/refactor/inline.py
@@ -591,3 +591,17 @@ assert x < limit, f'x must be < {limit}'
 -limit = 100
 -assert x < limit, f'x must be < {limit}'
 +assert x < 100, f'x must be < {100}'
+# -------------------------------------------------- await-expression
+async def f():
+    #? 4
+    a = await something()
+    return a + 1
+# ++++++++++++++++++++++++++++++++++++++++++++++++++
+--- inline.py
++++ inline.py
+@@ -1,5 +1,4 @@
+ async def f():
+     #? 4
+-    a = await something()
+-    return a + 1
++    return await something() + 1
